9cec79a8a96fa34fc384e09d7895376f7e61ee8b
Konstantin Buinak <kbuinak@gmail.com>
2018-12-15 13:13:18 +0100

    Changed some stuff in the RSA key generation algorithm. Now works on strings!

--- src/main/algorithms/cryptography/RSA.kt
+++ src/main/algorithms/cryptography/RSA.kt
@@ -20,14 +20,15 @@ private fun generateRandomCoprime(range: IntRange, number: Int): Int {
 }
 
 fun generateRandomKeys(primeRange: IntRange): Pair<RSAPublicKey, RSAPrivateKey> {
-    fun generateE(intRange: IntRange, n: Int, phi: Int): Int{
-        for (i in intRange){
+    fun generateE(intRange: IntRange, n: Int, phi: Int): Int {
+        for (i in intRange) {
             if (areCoprime(i.toLong(), n.toLong()) && areCoprime(i.toLong(), phi.toLong())) return i
         }
         return 1
     }
-    fun generateD(e: Int, phi: Int, intRange: IntRange): Int{
-        for (number in intRange){
+
+    fun generateD(e: Int, phi: Int, intRange: IntRange): Int {
+        for (number in intRange) {
             if ((number * e) % phi == 1) return number
         }
         return -1
@@ -44,8 +45,79 @@ fun generateRandomKeys(primeRange: IntRange): Pair<RSAPublicKey, RSAPrivateKey> 
     return Pair(public, private)
 }
 
+fun generateRandomKeys(primeRange: IntRange, minimumModulus: Int): Pair<RSAPublicKey, RSAPrivateKey> {
+    fun generateE(intRange: IntRange, n: Int, phi: Int): Int {
+        for (i in intRange) {
+            if (areCoprime(i.toLong(), n.toLong()) && areCoprime(i.toLong(), phi.toLong())) return i
+        }
+        return 1
+    }
+
+    fun generateD(e: Int, phi: Int, intRange: IntRange): Int {
+        for (number in intRange) {
+            if ((number * e) % phi == 1) return number
+        }
+        return -1
+    }
+
+    var n = 0
+    var phi = 0
+    while (n <= minimumModulus) {
+        generateRandomPrimePair(primeRange).run {
+            n = this.first * this.second
+            phi = (this.first - 1) * (this.second - 1)
+        }
+    }
+    val e = generateE(2 until phi, n, phi)
+    val d = generateD(e, phi, 2 until phi)
+
+    val private = RSAPrivateKey(n, d)
+    val public = RSAPublicKey(n, e)
+    return Pair(public, private)
+}
+
 fun rsaEncrypt(message: Int, publicKey: RSAPublicKey): Long {
-    return (raiseToPowerBig(message.toLong(), publicKey.exponent).mod(BigInteger.valueOf(publicKey.modulus.toLong())).toLong())
+    // Modulus must be bigger than the message to encrypt.
+    if (publicKey.modulus <= message) return -1
+    return (raiseToPowerBig(
+        message.toLong(),
+        publicKey.exponent
+    ).mod(BigInteger.valueOf(publicKey.modulus.toLong())).toLong())
+}
+
+fun rsaEncrypt(message: String, publicKey: RSAPublicKey): Pair<String, Int> {
+    var chunks = message
+        .map { char -> rsaEncrypt(char.toInt(), publicKey).toString() }
+
+    var longest = 0
+    chunks.forEach { if (it.length > longest) longest = it.length }
+    chunks = chunks.map { it ->
+        var result = it
+        while (result.length < longest) result = "0$result"
+        return@map result
+    }
+    val result = chunks.reduce { acc, s -> acc + s }
+    return Pair(result, longest)
+}
+
+fun rsaDecrypt(message: String, blockSize: Int, privateKey: RSAPrivateKey): String {
+    val chunks = message
+        .chunked(blockSize)
+        .map {
+            if (it[0] != '0') return@map it
+            else {
+                var result = it
+                while (result[0] == '0') {
+                    result = result.substring(1)
+                }
+                return@map result
+            }
+        }
+        .map { it -> rsaDecrypt(it.toLong(), privateKey).toChar() }
+    println()
+    var result = ""
+    chunks.forEach { result += it.toString() }
+    return result
 }
 
 fun rsaDecrypt(message: Long, privateKey: RSAPrivateKey): Long {
@@ -55,3 +127,13 @@ fun rsaDecrypt(message: Long, privateKey: RSAPrivateKey): Long {
 
 class RSAPublicKey(val modulus: Int, val exponent: Int)
 class RSAPrivateKey(val modulus: Int, val d: Int)
+
+
+fun main(args: Array<String>) {
+    var message = "Hello!"
+    val keys = generateRandomKeys(1..100, 200)
+    val result = rsaEncrypt(message, keys.first)
+    val decrypt = rsaDecrypt(result.first, result.second, keys.second)
+    println("Encrypting $message with key (mod = ${keys.first.modulus}, e = ${keys.first.exponent}) = ${result.first} with block size = ${result.second}")
+    println("Decrypting ${result.first} with block size ${result.second} with key (mod = ${keys.second.modulus}, d = ${keys.second.d}) = $decrypt")
+}
--- src/test/algorithms/cryptography/RSATest.kt
+++ src/test/algorithms/cryptography/RSATest.kt
@@ -1,16 +1,23 @@
 package test.algorithms.cryptography
 
+import junit.framework.Assert.assertEquals
+import main.algorithms.cryptography.generateRandomKeys
+import main.algorithms.cryptography.rsaDecrypt
+import main.algorithms.cryptography.rsaEncrypt
+import org.junit.Test
+import test.TestConstants
+import kotlin.random.Random
+
 class RSATest {
 
-    //FIX
-//    @Test
-//    fun rsaShouldWork() {
-//        for (i in 1..TestConstants.AMOUNT_OF_TESTS) {
-//            val keyPair = generateRandomKeys(2..100)
-//            val number = Random.nextInt(1, 20)
-//            val encrypted = rsaEncrypt(number, keyPair.first)
-//            val decrypted = rsaDecrypt(encrypted, keyPair.second)
-//            assertEquals(number, decrypted.toInt())
-//        }
-//    }
+    @Test
+    fun rsaShouldWork() {
+        for (i in 1..TestConstants.AMOUNT_OF_TESTS) {
+            val number = Random.nextInt(1, 1000)
+            val keyPair = generateRandomKeys(1..100, number)
+            val encrypted = rsaEncrypt(number, keyPair.first)
+            val decrypted = rsaDecrypt(encrypted, keyPair.second)
+            assertEquals(number, decrypted.toInt())
+        }
+    }
 }
