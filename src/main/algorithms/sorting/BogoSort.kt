09863be20427437cbf44083a1627cebc2212021d
Konstantin Buinak <kbuinak@gmail.com>
2018-12-17 19:48:37 +0100

    Commented bogo sort.

--- src/main/algorithms/sorting/BogoSort.kt
+++ src/main/algorithms/sorting/BogoSort.kt
@@ -12,6 +12,11 @@ package main.algorithms.sorting
  * @author Konstantin Buinak (https://github.com/buinak)
  */
 fun <T: Comparable<T>> bogoSort(list: MutableList<T>) {
+    /**
+     * Returns true if the list is sorted.
+     * Goes over every element and returns false if the order is not valid. In the end of the loop, in false has not
+     * been returned, returns true.
+     */
     fun isSorted(list: List<T>): Boolean {
         for (i in 1 until list.size) {
             if (list[i - 1] > list[i]) return false
@@ -19,6 +24,9 @@ fun <T: Comparable<T>> bogoSort(list: MutableList<T>) {
         return true
     }
 
+    /**
+     * Shuffle the list until it is sorted.
+     */
     while (!isSorted(list)) {
         list.shuffle()
     }
