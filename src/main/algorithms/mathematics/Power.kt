df6a21248c4774a2a9b219114be12697365a2153
Konstantin Buinak <kbuinak@gmail.com>
2018-12-20 14:20:02 +0100

    Power documented.

--- src/main/algorithms/mathematics/Power.kt
+++ src/main/algorithms/mathematics/Power.kt
@@ -2,6 +2,12 @@ package main.algorithms.mathematics
 
 import java.math.BigInteger
 
+/**
+ * Recursively raised a number to a certain power.
+ * This method returns the original number is power is 1, otherwise multiplies the number on the result of calling itself
+ * with decremented power.
+ * This means that the number will be multiplied on itself (the original) n amount of times, where n is the power.
+ */
 fun raiseToPower(num: Long, pow: Int): Long {
     return if (pow == 1) num
     else raiseToPower(num, pow - 1) * num
