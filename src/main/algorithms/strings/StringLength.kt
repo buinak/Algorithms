031edc62a0aa643febe037a4fc076f88d1bf5d75
Konstantin Buinak <kbuinak@gmail.com>
2018-12-28 18:18:16 +0100

    Some changes in the length code.

--- src/main/algorithms/strings/StringLength.kt
+++ src/main/algorithms/strings/StringLength.kt
@@ -3,12 +3,15 @@ package main.algorithms.strings
 fun stringLength(text: String): Int {
     var i = 0
     while (true){
-        var currChar: Char
         try {
-            currChar = text[i]
+            text[i]
         } catch (exception: IndexOutOfBoundsException){
             return i
         }
         i++
     }
+}
+
+fun main(args: Array<String>) {
+    println(stringLength("Tuna!"))
 }
