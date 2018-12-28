960a786fedf5b283c396638c8138cc04d079cc9d
Konstantin Buinak <kbuinak@gmail.com>
2018-12-28 17:06:44 +0100

    Implemented string search.

---
+++ src/main/algorithms/searching/StringSearch.kt
@@ -0,0 +1,18 @@
+package main.algorithms.searching
+
+fun search(text: String, pattern: String): Int {
+    //if the searched text is longer than the original
+    if (pattern.length > text.length) return -1
+    //if the searched text is null
+    if (pattern.length == 0) return 0
+
+    for (i in 0 until text.length){
+        var j = i
+        while (text[j] == pattern[j - i]){
+            if ((j - i) + 1 == pattern.length) return i
+            if (j + 1 == text.length) break
+            j++
+        }
+    }
+    return -1
+}
