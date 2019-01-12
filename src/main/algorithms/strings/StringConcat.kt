a3273c908812317dcfcbaf4c4f94f3640cb1fee0
Konstantin Buinak <kbuinak@gmail.com>
2019-01-12 22:35:32 +0100

    Added concatenation using char arrays.

--- src/main/algorithms/strings/StringConcat.kt
+++ src/main/algorithms/strings/StringConcat.kt
@@ -6,3 +6,14 @@ fun stringConcatenate(first: String, second: String): String {
     for (i in 0 until second.length) result += second[i]
     return result
 }
+
+fun stringConcatenateArrays(first: List<Char>, second: List<Char>): List<Char> {
+    var result = ArrayList<Char>(first.size + second.size)
+    var j = 0
+    for (i in 0 until first.size) i.run {
+        result[this] = first[this]
+        j = this
+    }
+    for (k in 0 until second.size) result[k + j + 1] = second[k]
+    return result
+}
